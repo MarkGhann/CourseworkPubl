e6b8f3963e39c45523b3325d7910c4cc1ba0647c
MarkGhann <gmarkmg@yandex.ru>
2020-05-11 11:45:53 +0300

    -

--- do.sh
+++ do.sh
@@ -2,12 +2,14 @@
 
 name=$1
 number=$2
+results=results$name
+mkdir $results
 cp tests/input$name-$number.xml .
 mv input$name-$number.xml input.xml
 for (( i=0; i < 100; i++ )) do
 python3 mainGA.py $number
 mv output.xml output$i.xml
-cp output$i.xml resultsB
+cp output$i.xml $results
 rm output$i.xml
 done
 rm input.xml
